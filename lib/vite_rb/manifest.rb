7840be5a9b0deb8c21814a73febed7450356a1d8
ParamagicDev <konnor5456@gmail.com>
2021-01-24 18:15:44 -0500

    add manifest testing

--- lib/vite_rb/manifest.rb
+++ lib/vite_rb/manifest.rb
@@ -5,8 +5,6 @@ require 'json'
 module ViteRb
   # The basic class for reading from the manifest.json
   class Manifest
-    VALID_TYPES = %i[js js.map css css.map].freeze
-
     attr_reader :hash
 
     def initialize(file)
@@ -17,26 +15,16 @@ module ViteRb
       @hash = load_manifest(manifest_file)
     end
 
-    def find_entrypoint(file_name, type)
-      @hash[:entrypoints][file_name.to_sym][type.to_sym]
-    end
-
     def find_file(file_name)
-      @hash[file_name.to_sym]
-    end
-
-    def find_chunk(chunk_name)
-      @hash[:chunks][chunk_name.to_sym][:js]
-    end
-
-    def self.valid_type?(type)
-      VALID_TYPES.include?(type)
+      @hash.fetch(file_name).fetch('file')
+    rescue KeyError
+      nil
     end
 
     private
 
     def load_manifest(manifest_file = ViteRb.config.manifest_file)
-      @hash = JSON.parse(File.read(manifest_file), symbolize_names: true)
+      @hash = JSON.parse(File.read(manifest_file))
     end
   end
 end
--- test/unit/manifest_test.rb
+++ test/unit/manifest_test.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 require 'json'
 
 MANIFEST_FILE = File.join(FIXTURE_DIR, 'manifest.json')
-MANIFEST_DATA = JSON.parse(File.read(MANIFEST_FILE), symbolize_names: true)
+MANIFEST_DATA = JSON.parse(File.read(MANIFEST_FILE))
 
 class ManifestTest < Minitest::Test
   def setup
@@ -19,23 +19,23 @@ class ManifestTest < Minitest::Test
     assert_equal ViteRb.manifest.hash, MANIFEST_DATA
   end
 
-  def test_finds_all_entrypoint_files
-    js = ViteRb.manifest.find_entrypoint(:application, :js)
-    js_map = ViteRb.manifest.find_entrypoint(:application, :"js.map")
-    css = ViteRb.manifest.find_entrypoint(:application, :css)
-    css_map = ViteRb.manifest.find_entrypoint(:application, :"css.map")
-
-    assert_equal js, '/entrypoints/application-9856bc23.js'
-    assert_equal js_map, '/entrypoints/application-9856bc23.js.map'
-    assert_equal css, '/css/application-4ceb08db.css'
-    assert_equal css_map, '/css/application-4ceb08db.css.map'
+  def test_find_bare_entry
+    application = "application.js"
+    file = ViteRb.manifest.find_file(application)
+    manifest_file = MANIFEST_DATA[application]["file"]
+    assert_equal file, manifest_file
   end
 
-  def test_find_file
-    app = ViteRb.manifest.find_file('entrypoints/application.js')
-    assert_equal app, '/entrypoints/application-9856bc23.js'
+  def test_find_nested_file
+    # nested files
+    nested = "nested/nested_entrypoint.js"
+    file = ViteRb.manifest.find_file(nested)
+    manifest_file = MANIFEST_DATA[nested]["file"]
 
-    non_existant = ViteRb.manifest.find_file('does-not-exist')
-    assert_nil non_existant
+    assert_equal file, manifest_file
+  end
+
+  def test_no_file_found
+    assert_nil ViteRb.manifest.find_file('does-not-exist')
   end
 end
